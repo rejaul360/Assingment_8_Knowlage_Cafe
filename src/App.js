66ceb0360ee45ac703508935580e8341eeffd34d
rejaul360 <rejaraju2k22@gmail.com>
2023-03-31 16:23:24 +0600

    added sideCard

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './Component/Navbar/Navbar';
 import Home from './Component/Home/Home';
+import SideCart from './Component/SideCart/SideCart';
 
 function App() {
   return (
@@ -13,8 +14,8 @@ function App() {
         <div className='col-md-8'>
           <Home></Home>
         </div>
-        <div className='col-md-4'>
-            <h1>hello</h1>
+        <div className='col-md-4 '>
+           <SideCart></SideCart>
         </div>
      </div>
      
---
+++ src/Component/SideCart/SideCart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SideCart.css'
+
+const SideCart = () => {
+    return (
+        <div className='   mt-2'>
+            <p className='d-flex align-items-center justify-content-center mt-2 secondCart'>Spent time on read : </p>
+        </div>
+    );
+};
+
+export default SideCart;
